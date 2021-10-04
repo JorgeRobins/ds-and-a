ec49b456e905098ec17cbc7318196734e81deda4
Jorge R. S. Robins <Jorge@JorgeRobins.co.uk>
2021-10-04 14:09:47 +0100

    DSA-010 Types of Sorting Questions

---
+++ Algorithms/Sorting/00_sortingTypeQuestions.js
@@ -0,0 +1,26 @@
+//#1 - Sort 10 schools around your house by distance:
+//A: Insertion Sort (small data set)
+
+//#2 - eBay sorts listings by the current Bid amount:
+//A: Radix or Counting Sort
+
+//#3 - Sport scores on ESPN
+//A: Quicksort (Could be different formats)
+
+//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data
+//A: Mergesort (distributed processing so guaranteed O(n log(n)) time)
+//A: Heapsort (worried about space complexity if not distributing)
+
+//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews
+//A: Insertion Sort (almost sorted data so should be quick)
+
+//#6 - Temperature Records for the past 50 years in Canada
+//A: Radix or Counting Sort
+//A: Quicksort
+
+//#7 - Large user name database needs to be sorted. Data is very random.
+//A: Mergesort (if enough memory since lower worst case time complexity)
+//A: Quicksort (if worried more about space than time)
+
+//#8 - You want to teach sorting for the first time
+//A: Bubble Sort, Selection Sort, Insertion Sort
